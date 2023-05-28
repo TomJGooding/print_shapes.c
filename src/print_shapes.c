fbbdb1e96ff8c962b0f20559243096eb1c7a3914
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-05-28 13:03:55 +0100

    add inverted pyramid

--- src/print_shapes.c
+++ src/print_shapes.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -21,10 +22,26 @@ void print_half_square(const int width) {
 }
 
 void print_sideways_triangle(const int width) {
-    const int height = width * 2 - 1;
+    const int height = (width * 2) - 1;
     for (int row = 1; row <= height; ++row) {
         int shape_span = width - abs(width - row);
         for (int col = 1; col <= shape_span; ++col) {
+            putchar('#');
+        }
+        putchar('\n');
+    }
+}
+
+void print_inverted_pyramid(const int width) {
+    assert(width % 2 != 0 && "pyramid width must be odd");
+    const int height = (width / 2) + 1;
+    for (int row = 0; row < height; ++row) {
+        int left_pad = row;
+        for (int pad = 0; pad < left_pad; ++pad) {
+            putchar(' ');
+        }
+        int shape_span = width - (left_pad * 2);
+        for (int span = 0; span < shape_span; ++span) {
             putchar('#');
         }
         putchar('\n');
@@ -41,5 +58,7 @@ int main(void) {
     printf("\nSIDEWAYS TRIANGLE\n\n");
     print_sideways_triangle(4);
 
+    printf("\nINVERTED PYRAMID\n\n");
+    print_inverted_pyramid(7);
     return 0;
 }
