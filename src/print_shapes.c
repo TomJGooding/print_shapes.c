0159e156e53252eee8b25a91f866696aa6ae9842
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-05-28 11:56:31 +0100

    add sideways triangle

--- src/print_shapes.c
+++ src/print_shapes.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void print_square(const int width) {
     for (int row = 0; row < width; ++row) {
@@ -18,12 +19,25 @@ void print_half_square(const int width) {
     }
 }
 
+void print_sideways_triangle(const int width) {
+    const int height = width * 2 - 1;
+    for (int row = 1; row <= height; ++row) {
+        for (int col = 1; col <= width - abs(width - row); ++col) {
+            putchar('#');
+        }
+        putchar('\n');
+    }
+}
+
 int main(void) {
     printf("\nSQUARE\n\n");
     print_square(5);
 
     printf("\nHALF SQUARE\n\n");
     print_half_square(5);
+
+    printf("\nSIDEWAYS TRIANGLE\n\n");
+    print_sideways_triangle(4);
 
     return 0;
 }
